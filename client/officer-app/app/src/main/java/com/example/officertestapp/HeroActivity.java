b1e83dc03f04b72cb5c60ab66d542cf4910db39c
Dhanushika Dharmasena <dhanushikadharmasena456@gmail.com>
2023-09-28 17:42:46 +0530

    Created the login page which shows to enter the contact number

--- client/officer-app/app/src/main/java/com/example/officertestapp/HeroActivity.java
+++ client/officer-app/app/src/main/java/com/example/officertestapp/HeroActivity.java
@@ -2,7 +2,9 @@ package com.example.officertestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HeroActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class HeroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hero);
+    }
+
+    public void hero_act_login_btn_handler(View v){
+        Intent i = new Intent(this,LoginActivity.class);
+        startActivity(i);
     }
 }
