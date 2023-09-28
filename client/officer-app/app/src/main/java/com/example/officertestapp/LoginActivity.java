645a77bb8425df0549c2cae4e4e1a698ab68f6ab
Dhanushika Dharmasena <dhanushikadharmasena456@gmail.com>
2023-09-29 05:11:52 +0530

    Created the login page which prompts to insert the OTP

--- client/officer-app/app/src/main/java/com/example/officertestapp/LoginActivity.java
+++ client/officer-app/app/src/main/java/com/example/officertestapp/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.officertestapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void login_otp_act_send_otp_btn_handler(View v){
+        Intent i = new Intent(this, LoginOtpActivity.class);
+        startActivity(i);
     }
 }
