896de1b3e90243e4ac4403cc57ad17c6f245bac8
Dhanushika Dharmasena <dhanushikadharmasena456@gmail.com>
2023-09-28 13:59:01 +0530

    Created the hero page

---
+++ client/officer-app/app/src/main/java/com/example/officertestapp/HeroActivity.java
@@ -0,0 +1,14 @@
+package com.example.officertestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HeroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hero);
+    }
+}
