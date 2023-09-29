c5e9a6e9c37f55a1e1dc432d57cf69c97d88d400
Dhanushika Dharmasena <dhanushikadharmasena456@gmail.com>
2023-09-29 15:58:57 +0530

    added the app bar to the fragments

--- client/officer-app/app/src/main/java/com/example/officertestapp/LoginOtpActivity.java
+++ client/officer-app/app/src/main/java/com/example/officertestapp/LoginOtpActivity.java
@@ -1,6 +1,8 @@
 package com.example.officertestapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,10 @@ public class LoginOtpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_otp);
+    }
+
+    public void login_otp_act_continue_btn_handler(View v){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
