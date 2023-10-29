2dacf779a8128e4609e91c8235904c0a903473d0
Uththara Jayawardana <gaveuththara1999@gmail.com>
2023-10-29 16:46:37 +0530

    Copied homeView.php from company-janindu to admin-uththara

---
+++ server/web/app/views/homeView.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="./style.css" rel="stylesheet" />
+	<title>Document</title>
+</head>
+
+<body>
+	<div class="navbar">
+		<img src="./assets/logo-black.png" alt="" class="navbar-logo">
+		<a href="./loginView.php">Login</a>
+	</div>
+	<div class="home-body">
+		<h1 class="header-text">Revolutionize <span class="font-primary">Parking <br>Management</br></span></h1>
+		<h2 class="sub-header-text">-PARK'N GO for Companies-</h2>
+		<p class="p-text">Welcome to the future of parking management in Sri Lanka! Experience a new era of efficiency and control with PARK'N GO. Unleash the power to effortlessly manage your parking spaces, supercharge your operations, and elevate the parking experience for your clients.</p>
+		<button class="black-button">Get Started</button>
+	</div>
+	<div class="main-section">
+		<h2 class="section-header">Our Services</h2>
+		<div class="card-section">
+			<div class="card card-1">
+				<div>
+					<h3>Total Parking Space Domainance</h3>
+				</div>
+			</div>
+			<div class="card card-2">
+				<div>
+					<h3>Instant Parking Officer Assignment</h3>
+				</div>
+
+			</div>
+			<div class="card card-3">
+				<div>
+					<h3>Real-time Space Status Insights</h3>
+				</div>
+
+			</div>
+			<div class="card card-4">
+				<div>
+					<h3>Unleash Productivity with Data</h3>
+				</div>
+			</div>
+			<div class="card card-5">
+				<div>
+					<h3>Effortless User-Friendly Interface</h3>
+				</div>
+
+			</div>
+		</div>
+		<button class="transparent-button">Get Started</button>
+	</div>
+	<div class="footer">
+		<p>Copyright &copy;2023 PARK'N GO. All rights reserved.</p>
+	</div>
+</body>
+
+</html>
