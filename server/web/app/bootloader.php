97e875ad7091eef06e2df00fd6909e159cb1523a
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-10-29 09:51:58 +0530

    build the base framework

--- server/web/app/bootloader.php
+++
@@ -1,5 +0,0 @@
-<?php
-    // requiring all files/classes to index.php
-    require_once "./config/config.php";
-    require_once "./libraries/Route.php"
-?>
--- server/web/app/libraries/Route.php
+++
@@ -1,6 +0,0 @@
-<?php
-    class Route{
-        // Route handling (find correct controller and call the method according to the url)
-        // server/web/public/index.php?url=
-    }
-?>
