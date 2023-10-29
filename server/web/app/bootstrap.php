df85a8cf2e1278c8b342801463d2ade806ea7eae
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-10-30 01:18:48 +0530

    did the most of the register part.

--- server/web/app/bootstrap.php
+++ server/web/app/bootstrap.php
@@ -3,6 +3,9 @@
 // Load Config
 require_once 'config/config.php';
 
+// Load Helpers
+require_once 'helpers/url_helper.php';
+
 // Autoload Core libraries
 spl_autoload_register(function ($className) {
   require_once 'libraries/' . $className . '.php';
