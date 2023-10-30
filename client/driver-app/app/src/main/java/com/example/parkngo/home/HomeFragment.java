db268d803f1fdb07d3964509afa6536927e32705
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2023-10-30 13:41:36 +0530

    implement main pages of all sections

--- client/driver-app/app/src/main/java/com/example/parkngo/home/HomeFragment.java
+++ client/driver-app/app/src/main/java/com/example/parkngo/home/HomeFragment.java
@@ -1,10 +1,12 @@
-package com.example.parkngo;
+package com.example.parkngo.home;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.parkngo.R;
 
 public class HomeFragment extends Fragment {
 
--- client/driver-app/app/src/main/java/com/example/parkngo/profile/ProfileFragment.java
+++ client/driver-app/app/src/main/java/com/example/parkngo/profile/ProfileFragment.java
@@ -1,23 +1,22 @@
-package com.example.parkngo;
+package com.example.parkngo.profile;
 
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-public class ScanFragment extends Fragment {
+import com.example.parkngo.R;
 
-    public ScanFragment() {
-        // Required empty public constructor
-    }
-
+public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scan, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
