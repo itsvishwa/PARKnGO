624ef0cb50004518448efb2f81c655eaf64ab3b3
Uththara Jayawardana <gaveuththara1999@gmail.com>
2023-10-31 22:40:27 +0530

    changes done for UI

--- server/web/app/controllers/Admins.php
+++ server/web/app/controllers/Admins.php
@@ -15,4 +15,16 @@ class Admins extends Controller
   {
     $this->view('admin/dashboardView');
   }
+  public function companiesView(){
+    $this->view('admin/companiesView');
+  }
+  public function deletionView(){
+    $this->view('admin/deletionView');
+  }
+  public function requestsHistoryView(){
+    $this->view('admin/requestsHistoryView');
+  }
+  public function requestsView(){
+    $this->view('admin/requestsView');
+  }
 }
