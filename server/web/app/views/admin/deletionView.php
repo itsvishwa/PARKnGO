4375caff7e576875c58b30c1aa14a0f3bb836e81
Uththara Jayawardana <gaveuththara1999@gmail.com>
2023-11-01 11:45:51 +0530

    Added log outs to the UIs

--- server/web/app/views/admin/deletionView.php
+++ server/web/app/views/admin/deletionView.php
@@ -19,7 +19,7 @@
                 <div>
                     <ul class="menu">
                         <li>
-                            <a href="./dashboardView.php">
+                            <a href="./dashboardView">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="menu-logo">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
                                 </svg>
@@ -76,6 +76,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                     </svg>
                     <a href="./dashboardView" class="company-name"><?php echo $_SESSION['user_name']; ?></a>
+                    <a href="../users/logout" class="logout">Log out</a>
                 </div>
             </div>
             <div class="business">
