cf0afb5e53c004c17008c24d3253ab6e26a60d65
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2023-11-08 19:45:17 +0530

    available parking spaces implemented

--- client/driver-app/app/src/main/java/com/example/parkngo/MainActivity.java
+++ client/driver-app/app/src/main/java/com/example/parkngo/MainActivity.java
@@ -5,15 +5,22 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.parkngo.home.AvailableParkingSpacesFragment;
 import com.example.parkngo.home.HomeFragment;
+import com.example.parkngo.home.NavigateFragment;
 import com.example.parkngo.parking.ParkingFragment;
 import com.example.parkngo.parking.ParkingSelectedFragment;
 import com.example.parkngo.profile.ProfileFragment;
 import com.example.parkngo.scan.ScanFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -58,6 +65,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void back_btn_parking_selected_handler(View view){
         replaceFragment(new ParkingFragment());
+    }
+
+    // home fragment -> view all available parking spaces viewer
+    public void home_fragment_view_available_spaces_btn_handler(View view) {
+        replaceFragment(new AvailableParkingSpacesFragment());
+    }
+
+    // proceed to navigation
+    public void available_parking_space_item_handler(View view){
+//        replaceFragment(new NavigateFragment());
+        Uri uri = Uri.parse("https://www.google.com/maps/dir/" + "university of colombo" + "/" + "Cmc - car park");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.setPackage("com.google.android.apps.maps");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    // back buttons
+    public void navigate_frag_back_btn_handler(View view){
+        replaceFragment(new AvailableParkingSpacesFragment());
+    }
+
+    public void ava_frag_back_btn_handler(View view){
+        replaceFragment(new HomeFragment());
     }
 
 }
