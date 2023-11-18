4b46eaa35bf3b50e6312a1d573e188edbbe9c37f
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2023-11-19 01:12:49 +0530

    added ENV file and .gitignore file

--- server/mobile/controllers/ParkingSpace.php
+++ server/mobile/controllers/ParkingSpace.php
@@ -4,6 +4,7 @@ class ParkingSpace extends Controller
 {
 
     private $parking_space_model;
+
     public function __construct()
     {
         $this->parking_space_model = $this->model("ParkingSpaceModel");
@@ -13,11 +14,11 @@ class ParkingSpace extends Controller
     // show only available parking spaces related to the given vehicle type
     public function view_available($vehicle_type)
     {
-        $result = $this->parking_space_model->get_available_parking_spaces();
+        $result = $this->parking_space_model->get_available_parking_spaces($vehicle_type);
 
-        if ($result === false) // no open parking spaces
+        if ($result === false) // no open parking spaces available for selected vehicle type
         {
-            $this->send_json_400("No open parking spaces, Please try again later");
+            $this->send_json_400("No parking spaces available at the movement for selected vehicle type");
         } else // there are open parking spaces
         {
             $spaces_data = []; // final array to send as a response
@@ -30,26 +31,114 @@ class ParkingSpace extends Controller
                     "latitude" => $space_data->latitude,
                     "longitude" => $space_data->longitude,
                     "is_public" => $space_data->is_public,
-                    "is_closed" => $space_data->is_closed,
-                    "companyy_id" => $space_data->company_id
+                    "free_slots" => $space_data->free_slots,
+                    "total_slots" => $space_data->total_slots,
+                    "rate" => $space_data->rate,
+                    "avg_star_count" => $space_data->avg_star_count,
+                    "total_review_count" => $space_data->total_review_count
                 ];
-                $data = null;
-                if ($vehicle_type === "bike") {
-                    $data = $space_data->bike_slots;
-                } elseif ($vehicle_type === "car") {
-                    $data = $space_data->car_slots;
-                } else {
-                    $data = $space_data->van_slots;
-                }
-                $data = explode(":", $data);
-                $temp["free_slots"] = $data[0];
-                $temp["total_slots"] = $data[1];
-                $temp["rate"] = $data[2];
-
                 $spaces_data[] = $temp; // add temp assosiative array to spaces_data[]
             }
 
             $this->send_json_200($spaces_data);
+        }
+    }
+
+
+    // show all parking spaces
+    public function view_all()
+    {
+        $result = $this->parking_space_model->get_all_parking_spaces();
+
+        if ($result === false) // no parking spaces 
+        {
+            $this->send_json_400("Sorry, No parking spaces available");
+        } else // have parking spaces
+        {
+            $spaces_data = [];
+
+            foreach ($result as $space_data) {
+                $temp = [
+                    "_id" => $space_data->_id,
+                    "name" => $space_data->name,
+                    "address" => $space_data->address,
+                    "is_public" => $space_data->is_public,
+                    "is_closed" => $space_data->is_closed,
+                    "avg_star_count" => $space_data->avg_star_count,
+                    "total_review_count" => $space_data->total_review_count
+                ];
+
+                $spaces_data[] = $temp; // add temp assosiative array to spaces_data[]
+
+            }
+
+            $this->send_json_200($spaces_data);
+        }
+    }
+
+
+    // show all details of the selected parking space
+    public function view($_id)
+    {
+        $parking_space_data = $this->parking_space_model->get_parking_space_details($_id);
+        $parking_slot_data = $this->parking_space_model->get_parking_space_status_details($_id);
+        $reviews_data = $this->parking_space_model->get_reviews_of_a_parking_space($_id);
+
+
+        if ($parking_space_data === false || $parking_slot_data === false) // no parking space for a given _id
+        {
+            $this->send_json_404("Parking Space Not Found");
+        } else // parking space found
+        {
+            $result = [
+                "name" => $parking_space_data->name,
+                "address" => $parking_space_data->address,
+                "latitude" => $parking_space_data->latitude,
+                "longitude" => $parking_space_data->longitude,
+                "is_public" => $parking_space_data->is_public,
+                "is_closed" => $parking_space_data->is_closed,
+                "avg_star_count" => $parking_space_data->avg_star_count,
+                "total_review_count" => $parking_space_data->total_review_count,
+                "slot_status" => null,
+                "reviews" => null
+            ];
+
+            $new_parking_slot_data = [];
+
+            foreach ($parking_slot_data as $slot_data) {
+                // add a slot to the new assosiative array
+                $new_parking_slot_data[] = [
+                    "vehicle_type" => $slot_data->vehicle_type,
+                    "free_slots" => $slot_data->free_slots,
+                    "total_slots" => $slot_data->total_slots,
+                    "rate" => $slot_data->rate
+                ];
+            }
+
+            // add new parking slot data array to final result array
+            $result["slot_status"] = $new_parking_slot_data;
+
+            if ($reviews_data === false) // no reviews yet
+            {
+                $result["reviews"] = "N/A";
+            } else // reviews found
+            {
+                $new_reviews_data = [];
+
+                foreach ($reviews_data as $review_data) {
+                    // add a review to the new assosiative array
+                    $new_reviews_data[] = [
+                        "name" => $review_data->first_name . " " . $review_data->last_name,
+                        "time_stamp" => $review_data->time_stamp,
+                        "no_of_stars" => $review_data->no_of_stars,
+                        "content" => $review_data->content,
+                    ];
+                }
+
+                $result["reviews"] = $new_reviews_data;
+            }
+
+            $this->send_json_200($result);
         }
     }
 }
--- server/mobile/models/ParkingSpaceModel.php
+++ server/mobile/models/ParkingSpaceModel.php
@@ -10,26 +10,118 @@ class ParkingSpaceModel
         }
 
         // get only available parking spaces
-        public function get_available_parking_spaces()
+        public function get_available_parking_spaces($vehicle_type)
         {
-                $this->db->query("SELECT * FROM parking_spaces WHERE is_closed = 0");
+                $this->db->query("SELECT
+                parking_spaces._id,
+                parking_spaces.name,
+                parking_spaces.address,
+                parking_spaces.latitude,
+                parking_spaces.longitude,
+                parking_spaces.is_public,
+                parking_spaces.avg_star_count,
+                parking_spaces.total_review_count,
+                parking_space_status.free_slots,
+                parking_space_status.total_slots,
+                parking_space_status.rate
+                FROM
+                        parking_spaces
+                JOIN
+                        parking_space_status ON parking_spaces._id = parking_space_status.parking_id
+                WHERE
+                        is_closed = 0
+                        AND 
+                        parking_space_status.vehicle_type = :vehicle_type");
+
+                $this->db->bind(":vehicle_type", $vehicle_type);
+
                 $result = $this->db->resultSet();
 
                 if ($result) {
-                        return [
-                                "_id" => $result->_id,
-                                "name" => $result->name,
-                                "latitude" => $result->latitude,
-                                "longitude" => $result->longitude,
-                                "no_of_slots" => $result->no_of_slots,
-                                "is_public" => $result->is_public,
-                                "is_closed" => $result->is_closed,
-                                "bike_slots" => $result->bike_slots,
-                                "car_slots" => $result->car_slots,
-                                "van_slots" => $result->van_slots,
-                                "companyy_id" => $result->company_id
-                        ];
+                        return $result;
                 } else // no open parking spaces
+                {
+                        return false;
+                }
+        }
+
+
+        // get all parking spaces
+        public function get_all_parking_spaces()
+        {
+                $this->db->query("SELECT * FROM parking_spaces");
+
+                $result = $this->db->resultSet();
+
+                if ($result) {
+                        return $result;
+                } else // no open parking spaces
+                {
+                        return false;
+                }
+        }
+
+
+        // get all details of a parking space for a given parking _id
+        public function get_parking_space_details($_id)
+        {
+                $this->db->query("SELECT * FROM parking_spaces WHERE _id = :_id");
+
+                $this->db->bind(":_id", $_id);
+
+                $result = $this->db->single();
+
+                if ($result) {
+                        return $result;
+                } else // no parking space for given id
+                {
+                        return false;
+                }
+        }
+
+        // get all details of a parking space status(slots) for a given parking _id
+        public function get_parking_space_status_details($_id)
+        {
+                $this->db->query("SELECT * FROM parking_space_status WHERE parking_id = :_id");
+
+                $this->db->bind(":_id", $_id);
+
+                $result = $this->db->resultSet();
+
+                if ($result) {
+                        return $result;
+                } else // no parking space for given id
+                {
+                        return false;
+                }
+        }
+
+
+        // get all details of a parking space status(slots) for a given parking _id
+        public function get_reviews_of_a_parking_space($_id)
+        {
+                $this->db->query("SELECT 
+                        review.time_stamp, 
+                        review.no_of_stars,
+                        review.content,
+                        driver.first_name,
+                        driver.last_name
+                FROM 
+                        review
+                JOIN
+                        driver
+                ON
+                        review.driver_id = driver._id
+                WHERE 
+                        review.parking_id = :_id");
+
+                $this->db->bind(":_id", $_id);
+
+                $result = $this->db->resultSet();
+
+                if ($result) {
+                        return $result;
+                } else // no parking space for given id
                 {
                         return false;
                 }
