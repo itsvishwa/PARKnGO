fff355ef7867c7f706975902ab49a1d0c6d488c6
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2023-12-08 15:05:05 +0530

    azure db set to off

--- server/mobile/config/config.php
+++ server/mobile/config/config.php
@@ -1,16 +1,16 @@
 <?php
 
 // DB Params - local
-// define('DB_HOST', 'localhost');
-// define('DB_USER', 'root');
-// define('DB_PASS', '');
-// define('DB_NAME', 'parkngo');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'parkngo');
 
 // Azure server
-define('DB_HOST', $_ENV['DB_HOST']);
-define('DB_USER', $_ENV['DB_USER']);
-define('DB_PASS', $_ENV['DB_PASSWORD']);
-define('DB_NAME', 'parkngo');
+// define('DB_HOST', $_ENV['DB_HOST']);
+// define('DB_USER', $_ENV['DB_USER']);
+// define('DB_PASS', $_ENV['DB_PASSWORD']);
+// define('DB_NAME', 'parkngo');
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
