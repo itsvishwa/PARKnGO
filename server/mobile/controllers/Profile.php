33ee7404bf72a99a0ec8c01326e20b323866bd5a
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2023-12-11 08:14:30 +0530

    payment history functionality implemented

--- server/mobile/controllers/Profile.php
+++ server/mobile/controllers/Profile.php
@@ -8,13 +8,16 @@ class Profile extends Controller
     private $profile_model;
     private $driver_model;
     private $otp_model;
+    private $payment_model;
     private $user_controller;
+
 
     public function __construct()
     {
         $this->profile_model = $this->model("ProfileModel");
         $this->driver_model = $this->model("DriverModel");
         $this->otp_model = $this->model("OTPModel");
+        $this->payment_model = $this->model("PaymentModel");
         $this->user_controller = new User;
     }
 
@@ -76,6 +79,44 @@ class Profile extends Controller
                 case 3:
                     $this->send_json_400("Invalid OTP");
                     break;
+            }
+        }
+    }
+
+
+    // send payment history of the driver
+    public function driver_payment_history()
+    {
+        $token_data = $this->verify_token_for_drivers();
+
+        if ($token_data === 400) {
+            $this->send_json_400("Invalid Token");
+        } elseif ($token_data === 404) {
+            $this->send_json_404("Token Not Found");
+        } else // token is valid
+        {
+            $payments_data = $this->payment_model->get_all_driver_payments_by_id($token_data["user_id"]);
+
+            if ($payments_data === false) // not payments yet
+            {
+                $this->send_json_400("No Payments has been made yet");
+            } else // there are payments data
+            {
+                $result_data = [];
+
+                foreach ($payments_data as $payment_data) {
+                    $temp = [
+                        "amount" => $payment_data->amount,
+                        "time_duration" =>  $payment_data->end_time - $payment_data->start_time,
+                        "vehicle_type" => $payment_data->vehicle_type,
+                        "vehicle_number" => $payment_data->vehicle_number,
+                        "parking_space_name" => $payment_data->name,
+                        "payment_time_stamp" => $payment_data->time_stamp
+                    ];
+                    $result_data[] = $temp;
+                }
+
+                $this->send_json_200($result_data);
             }
         }
     }
