659bf7aeba4fc0dffcdec9953a5bffb87db1c968
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2023-12-11 08:14:30 +0530

    payment history functionality implemented

--- server/mobile/models/PaymentModel.php
+++ server/mobile/models/PaymentModel.php
@@ -9,6 +9,7 @@ class PaymentModel
         $this->db = new Database;
     }
 
+    // get all data of a selected payment
     public function get_all_data($payment_id)
     {
         $this->db->query(
@@ -69,6 +70,47 @@ class PaymentModel
             return true;
         } else {
             return false;
+        }
+    }
+
+
+    // get driver payments by a selected 
+    public function get_all_driver_payments_by_id($driver_id)
+    {
+        $this->db->query(
+            "SELECT 
+            payment.amount,
+            payment.payment_method,
+            payment.time_stamp,
+            parking_session.start_time, 
+            parking_session.end_time,
+            parking_session.vehicle_number,
+            parking_session.vehicle_type,
+            parking_spaces.name
+            FROM 
+                payment
+            JOIN 
+                parking_session
+            ON
+                payment.session_id = parking_session._id 
+            JOIN
+                parking_spaces
+            ON
+                parking_session.parking_id = parking_spaces._id 
+            WHERE 
+                payment.driver_id = :driver_id
+                AND
+                payment.is_complete = 1"
+        );
+
+        $this->db->bind(":driver_id", $driver_id);
+
+        $result = $this->db->resultSet();
+
+        if ($result === []) {
+            return false;
+        } else {
+            return $result;
         }
     }
 }
