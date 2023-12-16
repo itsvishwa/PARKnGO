d561ff5c911f531080383f9f2aeffccf2a49833a
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-12-16 11:42:39 +0530

    did the home page design

--- server/web/app/views/homeView.php
+++ server/web/app/views/homeView.php
@@ -14,7 +14,7 @@
     <div class="hero-header">
       <a href="../"><img src="<?php echo URLROOT; ?>/css/assets/logo-white.png" alt="logo" width="200px" /></a>
       <a href="./users/loginView">
-        <button class="login-btn">
+        <button class="get-started-btn btn2">
           Login
         </button>
       </a>
@@ -63,64 +63,155 @@
     </div>
     <div class="feature-sub-section">
       <div class="feature-text left">
-        <h1 class="font-primary">Real Time Parking Space Management</h1>
-        <p>Experience the power to monitor and control your parking spaces dynamically. Instantly view the occupancy status, track parking-officer activities in real time and many more.</p>
-        <p>Say goodbye to manual reporting with our cutting-edge Park'n Go system.</p>
+        <h1 class="font-primary">Mobile Empowerment for Parking Officers</h1>
+        <p>Equip your parking officers with Park'nGo's mobile app.</p>
+        <p>A powerful tool for real-time updates, efficient task management, and instant communication. Enhance operational agility, address issues promptly, and ensure a seamless parking experience.</p>
       </div>
       <div class="feature-img mr-60">
-        <img src="<?php echo URLROOT; ?>/css/assets/features/realtime.png" alt="realtime parking space management">
+        <img src="<?php echo URLROOT; ?>/css/assets/features/mobile.png" alt="realtime parking space management">
       </div>
     </div>
     <div class="feature-sub-section">
       <div class="feature-img ml-60">
-        <img src="<?php echo URLROOT; ?>/css/assets/features/realtime.png" alt="realtime parking space management">
+        <img src="<?php echo URLROOT; ?>/css/assets/features/revenue.png" alt="realtime parking space management">
       </div>
       <div class="feature-text right">
-        <h1 class="font-primary">Real Time Parking Space Management</h1>
-        <p>Experience the power to monitor and control your parking spaces dynamically. Instantly view the occupancy status, track parking-officer activities in real time and many more.</p>
-        <p>Say goodbye to manual reporting with our cutting-edge Park'n Go system.</p>
+        <h1 class="font-primary">Strategic Revenue Analysis for Growth Network Expansion for Increased Visibility</h1>
+        <p>Park'n Go's advanced analytics propel your company toward intelligent, data-driven revenue strategies. Identify trends, optimize pricing, and uncover new streams for sustainable growth.</p>
+        <p>Transform your parking spaces into lucrative assets with informed decision-making backed by comprehensive data.</p>
       </div>
     </div>
     <div class="feature-sub-section">
       <div class="feature-text left">
-        <h1 class="font-primary">Real Time Parking Space Management</h1>
-        <p>Experience the power to monitor and control your parking spaces dynamically. Instantly view the occupancy status, track parking-officer activities in real time and many more.</p>
-        <p>Say goodbye to manual reporting with our cutting-edge Park'n Go system.</p>
+        <h1 class="font-primary">Network Expansion for Increased Visibility</h1>
+        <p>Leverage Park'n Go's island-wide reach to connect with a diverse customer base. Become a central hub for parking convenience, boosting footfall and utilization.</p>
+        <p>Enhance your company's reputation and ensure constant demand for your parking spaces, creating new avenues for revenue generation and business success.</p>
       </div>
       <div class="feature-img mr-60">
-        <img src="<?php echo URLROOT; ?>/css/assets/features/realtime.png" alt="realtime parking space management">
+        <img src="<?php echo URLROOT; ?>/css/assets/features/network.png" alt="realtime parking space management">
       </div>
     </div>
 
   </div>
-  <div class="package-section ">
+  <div class="package-section">
     <h1 class="center package-header">Flexible pricing<br>
       for companies of all sizes</h1>
     <div class="package-cards">
       <div class="package-card">
-        <h1>Rs.399/<span>mon</span></h1>
-        <h2>Plus</h2>
+        <h1 class="text-center package-card1">Rs.399/<span class="package-duration">mon</span><br><span class="package-name">Plus</span></h1>
+        <div class="includes">
+          <h3>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="verified">
+              <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+            </svg>
+            up to <span class="span1">2</span> Parking Spaces
+          </h3>
+          <h3>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="verified">
+              <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+            </svg>
+            up to <span class="span1">5</span> Parking Officers
+          </h3>
+          <h3>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="verified">
+              <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+            </svg>
+            Parking Officer Mobile App
+          </h3>
+          <h3>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="verified">
+              <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+            </svg>
+            Revenue Analysis
+          </h3>
+        </div>
+        <a href=""><button class="get-started-btn btn1">Get Started</button></a>
+      </div>
+      <div class="package-card">
+        <h1 class="text-center package-card2">Rs.699/<span class="package-duration">mon</span><br><span class="package-name">Pro</span></h1>
 
         <div class="includes">
           <h3>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="verified">
               <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
             </svg>
-            up to 2 Parking Spaces
+            up to <span class="span2">5</span> Parking Spaces
           </h3>
           <h3>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="verified">
               <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
             </svg>
-            up to 5 Parking Officers
+            up to <span class="span2">15</span> Parking Officers
+          </h3>
+          <h3>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="verified">
+              <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+            </svg>
+            Parking Officer Mobile App
+          </h3>
+          <h3>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="verified">
+              <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+            </svg>
+            Revenue Analysis
           </h3>
         </div>
-
-
-        <a href=""><button>Get Started</button></a>
+        <a href=""><button class="get-started-btn btn2">Get Started</button></a>
       </div>
-      <div class="package-card"></div>
-      <div class="package-card"></div>
+      <div class="package-card">
+        <h1 class="text-center package-card3">Rs.999/<span class="package-duration">mon</span><br><span class="package-name">Enterprise</span></h1>
+        <div class="includes">
+          <h3>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="verified">
+              <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+            </svg>
+            up to <span class="span3">25</span> Parking Spaces
+          </h3>
+          <h3>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="verified">
+              <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+            </svg>
+            up to <span class="span3">100</span> Parking Officers
+          </h3>
+          <h3>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="verified">
+              <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+            </svg>
+            Parking Officer Mobile App
+          </h3>
+          <h3>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="verified">
+              <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+            </svg>
+            Revenue Analysis
+          </h3>
+        </div>
+        <a href=""><button class="get-started-btn btn3">Get Started</button></a>
+      </div>
+    </div>
+  </div>
+  <div class="get-started-section">
+    <p><span>Revolutionize your parking management experience with us!</span> Elevate your business effortlessly as you take control of every parking space in realtime. Unleash the power to optimize and monitor your parking operations seamlessly</p>
+    <a href="./users/loginView"><button class="hero-btn">Get Started</button></a>
+  </div>
+  <div class="footer-section">
+    <div class="footer-top">
+      <div class="footer-left">
+        <img src="<?php echo URLROOT; ?>/css/assets/logo-white.png" alt="" class="footer-logo">
+        <div class="qr">
+          <p><i>Scan Me</i></p>
+          <img src="<?php echo URLROOT; ?>/css/assets/qr.png" alt="" class="qr">
+        </div>
+      </div>
+      <div class="footer-right">
+        <a href="">About Us</a>
+        <a href="">Contact Us</a>
+        <a href="">Customer Care</a>
+        <a href="">Privacy Policy</a>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      Copyright &copy; 2023 PARKnGO. All Rights Reserved.
     </div>
   </div>
 </body>
