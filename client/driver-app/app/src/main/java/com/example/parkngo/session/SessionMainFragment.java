3a1b007aaf1ca84d12a5b7de64bf06cbe75090e0
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2024-01-10 11:32:24 +0530

    main session btn handlers implemented

--- client/driver-app/app/src/main/java/com/example/parkngo/session/SessionMainFragment.java
+++ client/driver-app/app/src/main/java/com/example/parkngo/session/SessionMainFragment.java
@@ -1,20 +1,45 @@
 package com.example.parkngo.session;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.parkngo.R;
+import com.example.parkngo.session.helpers.SessionMainButtonHandlers;
 
 public class SessionMainFragment extends Fragment {
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_session_main, container, false);
+        View sessionMainView = inflater.inflate(R.layout.fragment_session_main, container, false);
+
+        SessionMainButtonHandlers sessionMainButtonHandlers = new SessionMainButtonHandlers(getContext(), sessionMainView);
+
+
+        // onclick listeners.................................................................................
+        sessionMainButtonHandlers.initVehicleBtnHandlers();
+
+        Button continueBtn = sessionMainView.findViewById(R.id.fragment_session_main_continue_btn);
+        continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "clicked " + sessionMainButtonHandlers.getSelectedVehicle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        // onclick listeners.................................................................................
+
+        return sessionMainView;
     }
 }
