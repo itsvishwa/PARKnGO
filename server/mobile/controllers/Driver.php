c53892d21456766823195caccede6184b25eb2e3
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2024-01-31 16:54:24 +0530

    edit/delete fucntionalities are completed for driver-qr-vehicle

--- server/mobile/controllers/Driver.php
+++ server/mobile/controllers/Driver.php
@@ -76,4 +76,72 @@ class Driver extends Controller
                         $this->send_json_200("Vehicle details are added successfully!");
                 }
         }
+
+        public function edit_vehicle()
+        {
+                $vehicle_data = [
+                        "vehicle_name" => trim($_POST["vehicle_name"]),
+                        "vehicle_number" => trim($_POST["vehicle_number"]),
+                        "vehicle_type" => trim($_POST["vehicle_type"]),
+                        "selected" => trim($_POST["selected"])
+                ];
+
+                $token_data = $this->verify_token_for_drivers();
+
+                if ($token_data === 400) {
+                        $this->send_json_400("Invalid Token");
+                } else if ($token_data === 404) {
+                        $this->send_json_404("Token not found");
+                } else {
+                        $result = $this->driver_qr_model->check_user($token_data["user_id"], $vehicle_data["selected"]);
+                        if ($result) {
+                                $this->driver_qr_model->update_vehicle_info(
+                                        $vehicle_data["vehicle_name"],
+                                        $vehicle_data["vehicle_number"],
+                                        $vehicle_data["vehicle_type"],
+                                        $vehicle_data["selected"],
+                                        $token_data["user_id"]
+                                );
+                                $this->send_json_200("Vehicle information has been updated");
+                        } else {
+                                $this->send_json_400("Invalid user details");
+                        }
+                }
+        }
+
+        public function delete_vehicle($selected_vehicle)
+        {
+                $token_data = $this->verify_token_for_drivers();
+
+                if ($token_data === 400) {
+                        $this->send_json_400("Invalid Token");
+                } else if ($token_data === 404) {
+                        $this->send_json_404("Token not found");
+                } else {
+                        $result = $this->driver_qr_model->check_user($token_data["user_id"], $selected_vehicle);
+                        if ($result) {
+                                $this->driver_qr_model->delete_driver_vehicle($selected_vehicle, $token_data["user_id"]);
+                                // has to restructure reamainng vehicle informations
+                                if ($selected_vehicle == 2) {
+                                        $result = $this->driver_qr_model->check_user($token_data["user_id"], 3);
+                                        if ($result) {
+                                                $this->driver_qr_model->update_driver_selected_vehicle_level($token_data["user_id"], 3, 2);
+                                        }
+                                } else if ($selected_vehicle == 1) {
+                                        $result = $this->driver_qr_model->check_user($token_data["user_id"], 2);
+                                        if ($result) {
+                                                $this->driver_qr_model->update_driver_selected_vehicle_level($token_data["user_id"], 2, 1);
+                                        }
+                                        $result = $this->driver_qr_model->check_user($token_data["user_id"], 3);
+                                        if ($result) {
+                                                $this->driver_qr_model->update_driver_selected_vehicle_level($token_data["user_id"], 3, 2);
+                                        }
+                                }
+                                $this->send_json_200("Vehicle information has been deleted successfully!");
+                        } else {
+                                // driver don't have a vehicle information related to requested data
+                                $this->send_json_400("Invald user details");
+                        }
+                }
+        }
 }
