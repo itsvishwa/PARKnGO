53aa85a4cc2911c79ef8fadb6440035b7dfff981
Dhanushika Dharmasena <dhanushikadharmasena456@gmail.com>
2024-02-14 05:22:02 +0530

    Parking status integrated commit

--- client/officer-app/app/src/main/java/com/example/officertestapp/Status/ParkingStatusModel.java
+++ client/officer-app/app/src/main/java/com/example/officertestapp/Status/ParkingStatusModel.java
@@ -5,26 +5,33 @@ public class ParkingStatusModel {
     String vehicleType;
     String dateTime;
     String parkingStatus;
+    String _id;
 
-
-    public ParkingStatusModel(String vehicleNumber, String vehicleType, String dateTime, String parkingStatus) {
+    public ParkingStatusModel(String _id, String vehicleNumber, String vehicleType, String dateTime, String parkingStatus) {
         this.vehicleNumber = vehicleNumber;
         this.vehicleType = vehicleType;
         this.dateTime = dateTime;
         this.parkingStatus = parkingStatus;
+        this._id = _id;
     }
 
-    public String getVehicleNumber() {return vehicleNumber;}
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
 
     public String getVehicleType() {
         return vehicleType.toUpperCase();
     }
 
-    public String getDateTime() {return dateTime;}
+    public String getDateTime() {
+        return dateTime;
+    }
 
     public String getParkingStatus() {
         return parkingStatus;
     }
 
-
+    public String getID(){
+        return _id;
+    }
 }
