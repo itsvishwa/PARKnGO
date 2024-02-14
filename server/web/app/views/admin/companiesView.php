7405c060e8fdeb934a0f0ec51f8791675b67f768
Uththara Jayawardana <gaveuththara1999@gmail.com>
2024-02-14 22:13:37 +0530

    remove some unnecessary details from companiesView

--- server/web/app/views/admin/companiesView.php
+++ server/web/app/views/admin/companiesView.php
@@ -89,18 +89,7 @@
         <div class="heading">
         </div>
       </div>
-      <!--<div class="b-card">
-        <div class="b-card-content p text-grey">
-          <p> Search a company</p> 
-          <input type="text" id="companySearch" placeholder="Search a Company" />
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="search-logo">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-          </svg> 
-
-        </div>
-      </div>-->
-
-      <!--  <div class="b-card">-->
+     
       <div class="b-card-content p text-grey">
         <div class="search-box">
           <input type="text" id="companySearch" placeholder="Search a Company" />
