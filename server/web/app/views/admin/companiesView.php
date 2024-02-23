e89da5909653b38c07580f52d255e29783f12ebc
Uththara Jayawardana <gaveuththara1999@gmail.com>
2024-02-23 11:22:53 +0530

    Change the download and delete buttons styles in the companiesView

--- server/web/app/views/admin/companiesView.php
+++ server/web/app/views/admin/companiesView.php
@@ -244,11 +244,11 @@
                    
                     <button class="b-view-more bg-black" type="button" onclick="downloadDocument(<?php echo ($application->_id); ?>)">
                       <div class="b-card_down-icon ">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" class="rd-menu-logo">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" class="rd-menu-logo">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
                         </svg>
                       </div>
-                      <div class="b-view-more-card2-content text-white">
+                      <div class="b-view-more-card2-content text-black">
                         <p>Download the document</p>
                       </div>
                     </button>
@@ -258,12 +258,12 @@
                     <button class="b-delete-more bg-black" onclick="confirmDelete(<?php echo $application->_id; ?>)">
 
                       <div class="b-card_del-icon ">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="delete-logo">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="red" class="delete-logo">
                           <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clipRule="evenodd" />
                         </svg>
                       </div>
 
-                      <div class="b-delete-more-card2-content text-white">
+                      <div class="b-delete-more-card2-content text-red">
                         <p>Delete</p>
                       </div>
 
