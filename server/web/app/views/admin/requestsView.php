ae73bea9f1746c5ac407e1261d8b1d2a46e70fdc
Uththara Jayawardana <gaveuththara1999@gmail.com>
2024-02-23 14:29:03 +0530

    Changed the gap between pending requests and the icon

--- server/web/app/views/admin/requestsView.php
+++ server/web/app/views/admin/requestsView.php
@@ -78,7 +78,7 @@
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="menu-logo">
             <path fillRule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
           </svg>
-          <h3>Pending Requests</h3>
+          <h3 class="ml-5">Pending Requests</h3>
         </div>
         <div class="profile">
          <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="menu-logo mr">
