8858f9b5e902ef91207e88cefaf6ef25b941fd1a
Dhanushika Dharmasena <dhanushikadharmasena456@gmail.com>
2024-04-23 18:42:49 +0530

    updated UI content commit

--- client/officer-app/app/src/main/java/com/example/officertestapp/ForceEnd/ForceEndMainFragment.java
+++ client/officer-app/app/src/main/java/com/example/officertestapp/ForceEnd/ForceEndMainFragment.java
@@ -8,9 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.example.officertestapp.ForceEnd.Helpers.ForceEndMainSearchHelper;
 import com.example.officertestapp.ForceEnd.Helpers.ForceEndedFetchData;
+import com.example.officertestapp.Helpers.ParkngoStorage;
 import com.example.officertestapp.MainActivity;
 import com.example.officertestapp.R;
 
@@ -23,6 +25,20 @@ public class ForceEndMainFragment extends Fragment {
         View forceEndsessionsView = inflater.inflate(R.layout.fragment_force_end_main, container, false);
 
         loadingView = inflater.inflate(R.layout.loading_frag, container, false);
+
+        //Create an instance of Parkngo Storage using the Fragment's context
+        ParkngoStorage parkngoStorage = new ParkngoStorage(getContext());
+
+        // Get data from storage
+        String parkingName = parkngoStorage.getData("parkingName");
+
+        // Find the TextView by its ID
+        TextView parkingNameView = forceEndsessionsView.findViewById(R.id.force_end_frag_parking_name);
+
+        // Set the officer's name in the TextView
+        parkingNameView.setText(parkingName);
+
+
 
         // fetching data
         new ForceEndedFetchData(forceEndsessionsView, loadingView, getContext());
