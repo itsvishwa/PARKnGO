6719d70e7fa0e853baa01d2ff54b20095289580f
Dhanushika Dharmasena <dhanushikadharmasena456@gmail.com>
2024-04-23 18:42:49 +0530

    updated UI content commit

--- client/officer-app/app/src/main/java/com/example/officertestapp/Profile/ProfilePaymentHistoryFragment.java
+++ client/officer-app/app/src/main/java/com/example/officertestapp/Profile/ProfilePaymentHistoryFragment.java
@@ -7,7 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.officertestapp.Helpers.ParkngoStorage;
 import com.example.officertestapp.MainActivity;
 import com.example.officertestapp.Profile.Helpers.PPHFetchData;
 
@@ -26,6 +28,18 @@ public class ProfilePaymentHistoryFragment extends Fragment {
         // Inflate the layout for this fragment
         paymentHistoryView  = inflater.inflate(R.layout.fragment_profile_payment_history, container, false);
         loadingView = inflater.inflate(R.layout.loading_frag, container, false);
+
+        //Create an instance of Parkngo Storage using the Fragment's context
+        ParkngoStorage parkngoStorage = new ParkngoStorage(getContext());
+
+        // Get data from storage
+        String parkingName = parkngoStorage.getData("parkingName");
+
+        // Find the TextView by its ID
+        TextView parkingNameView = paymentHistoryView.findViewById(R.id.profile_frag_app_bar_parking_name);
+
+        // Set the officer's name in the TextView
+        parkingNameView.setText(parkingName);
 
         // fetching data
         new PPHFetchData(paymentHistoryView, loadingView, getContext(), (MainActivity)requireContext());
