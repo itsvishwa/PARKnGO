9a0710ee0dd3b602e03746a374a009fdad2efaa7
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2024-04-23 22:59:18 +0530

    search bar errors are fixed

--- client/driver-app/app/src/main/java/com/example/parkngo/home/AvailableParkingSpacesFragment.java
+++ client/driver-app/app/src/main/java/com/example/parkngo/home/AvailableParkingSpacesFragment.java
@@ -1,9 +1,15 @@
 package com.example.parkngo.home;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +18,7 @@ import android.widget.Toast;
 
 import com.example.parkngo.MainActivity;
 import com.example.parkngo.R;
+import com.example.parkngo.helpers.LocationHelper;
 import com.example.parkngo.home.helpers.AvailableParkingSpaceHelper;
 import com.example.parkngo.home.helpers.AvailableParkingSpaceModel;
 
@@ -26,6 +33,7 @@ public class AvailableParkingSpacesFragment extends Fragment {
     String vehicleType;
     MainActivity mainActivity;
     ArrayList<AvailableParkingSpaceModel> availableParkingSpaceModelsArr  = new ArrayList<>();
+    Context context;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,6 +42,7 @@ public class AvailableParkingSpacesFragment extends Fragment {
         availableParkingSpaceView  = inflater.inflate(R.layout.fragment_available_parking_spaces, container, false);
         loadingView = inflater.inflate(R.layout.loading_frag, container, false);
         errorView = inflater.inflate(R.layout.fragment_error, container, false);
+
 
         // store passed data
         if (getArguments() != null) {
