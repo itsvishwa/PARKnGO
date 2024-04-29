6186eb0f13af301333148fd1726e9a4f8492bcae
janindu-kulathilka <86515184+janindu-kulathilaka@users.noreply.github.com>
2024-04-29 15:00:17 +0530

    logout error fixed

--- server/web/app/views/company/parkingSpaceCloseView.php
+++ server/web/app/views/company/parkingSpaceCloseView.php
@@ -86,7 +86,7 @@
 
         <div class="profile">
           <a href="./dashboardView" class="company-name"><?php echo $_SESSION['user_name']; ?></a>
-          <a href="../users/logout" class="logout">Log out</a>
+          <a href="../../users/logout" class="logout">Log out</a>
         </div>
       </div>
       <div class="center">
